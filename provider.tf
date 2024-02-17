fab57cd1d7345cb50749cc74325a5b419d2212ee
veerendra Naidu <naiduveerendra@gmail.com>
2024-02-17 12:24:05 +0800

    updated provider

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
-provider "aws"{
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
+
 terraform {
-    required_providers {
-      aws = {
-        source = "hasicorp/aws"
-        version = "~> 3.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
